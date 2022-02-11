d8dcc75d37653aa0a8771180698e06f415e4b733
Okiri Gabriel <okirigabriel24@gmail.com>
2022-02-10 16:39:08 -1000

    fixed app.js interface

--- app.js
+++ app.js
@@ -200,3 +200,17 @@ myLastSpan.style = "display:block;font-size:20px;font-weight:normal;text-transfo
 
 
 
+
+
+// var express = require('express')
+// var app = express()
+
+// app.get('/', function (req, res) {
+// 	res.send('testing auto scaling')
+// })
+
+// var server = app.listen(3000, function () {
+// 	var host = server.address().address
+// 	var port = server.address().port
+// 	console.log('Server listening at http://%s:%s', host, port)
+// })
